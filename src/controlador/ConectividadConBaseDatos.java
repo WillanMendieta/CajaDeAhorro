b1cfdd1b909712516ff6b2a44d386d7e41779ba7
Willan Mendieta <wmendietam@est.ups.edu.ec>
2022-01-05 14:41:30 -0500

    Conexión
    Se creo la carpetas y bla clase de conexión en la carpeta de controlador

--- src/controlador/ConectividadConBaseDatos.java
+++ src/controlador/ConectividadConBaseDatos.java
@@ -1,5 +1,32 @@
 package controlador;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConectividadConBaseDatos {
 
+	public static void main(String[] args) {
+
+		Connection conexion = null;
+		try {
+			conexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/CajaDeAhorro", "mendietaP",
+					"mendietaP");
+
+			if (conexion != null) {
+				System.out.print("Conexion establecida...");
+
+			} else {
+
+				System.out.print("Conexion fallida");
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+	}
+
 }
+
+
+
