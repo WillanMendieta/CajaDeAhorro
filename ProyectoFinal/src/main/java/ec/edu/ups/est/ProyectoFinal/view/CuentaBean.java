bf1b4e14fe558681ab07ffa42f8b6f821da935e4
VásquezByron1 <bvasquezs@est.ups.edu.ec>
2022-01-06 21:11:25 -0500

    Tipos de Cuenta

--- ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/view/CuentaBean.java
+++ ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/view/CuentaBean.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.Random;
 
 import ec.edu.ups.est.ProyectoFinal.business.CuentaON;
 import ec.edu.ups.est.ProyectoFinal.model.Cuenta;
@@ -38,6 +39,12 @@ public class CuentaBean implements Serializable {
 	public String guardarCuenta() {
 		cuentaON.insertarCuenta(newcuenta);
 
+		return null;
+	}
+	
+	public String generarNumCuenta() {
+		/*Random rnd = new Random();
+        System.out.print("Número aleatorio real entre [0,1[ : "+rnd.nextDouble());*/
 		return null;
 	}
 }
