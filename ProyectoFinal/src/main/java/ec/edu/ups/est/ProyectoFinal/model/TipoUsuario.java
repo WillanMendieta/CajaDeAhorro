4d899df990d7ace5829b7c593535b327fc087b66
José Quinde <flakoguille1996@gmail.com>
2022-01-20 23:12:41 -0500

    Update TipoUsuario.java

--- ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/model/TipoUsuario.java
+++ ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/model/TipoUsuario.java
@@ -1,5 +1,41 @@
 package ec.edu.ups.est.ProyectoFinal.model;
 
-public class TipoUsuario {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_Tipo_Usuario")
+public class TipoUsuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "tip_usu_id")
+	private int id;
+
+	@Column(name = "tip_usu_nombre")
+	private String nombre;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
 }
