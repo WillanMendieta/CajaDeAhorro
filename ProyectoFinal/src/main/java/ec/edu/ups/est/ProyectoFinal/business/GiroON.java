82f13d3556904a17a42f2c242bb4fdff9b7c7777
VásquezByron1 <bvasquezs@est.ups.edu.ec>
2022-02-03 23:04:15 -0500

    Clase ON arreglada

--- ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/GiroON.java
+++ ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/GiroON.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import ec.edu.ups.est.ProyectoFinal.dao.CiudadDAO;
 import ec.edu.ups.est.ProyectoFinal.dao.GiroDAO;
+import ec.edu.ups.est.ProyectoFinal.model.Ciudad;
 import ec.edu.ups.est.ProyectoFinal.model.Giro;
 
 @Stateless
@@ -13,19 +15,27 @@ public class GiroON {
 
 	@Inject
 	private GiroDAO giroDAO;
-	
-	public void insertarGiro(Giro g) throws Exception{
+
+	@Inject
+	private CiudadDAO ciudadDAO;
+
+	public void insertarGiro(Giro g) throws Exception {
 		giroDAO.insert(g);
+		
 	}
-	
-	public List<Giro> getGiros(){
+
+	public List<Giro> getGiros() {
 		return giroDAO.getList();
 	}
-	
+
 	public Giro getGiros(int codigo) {
 		return giroDAO.read(codigo);
 	}
-	
-	
 
+	public Ciudad getCiudad(int codigo) {
+
+		Ciudad ciudad = ciudadDAO.read(codigo);
+		return ciudad;
+
+	}
 }
