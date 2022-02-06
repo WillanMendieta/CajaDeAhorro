cd8728909243a12f0330bf76dfed99d5d392f50c
Darwin <dleont@est.ups.edu.ec>
2022-02-06 01:53:56 -0500

    Create PolizaON

---
+++ ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/PolizaON.java
@@ -0,0 +1,59 @@
+package ec.edu.ups.est.ProyectoFinal.business;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import ec.edu.ups.est.ProyectoFinal.dao.CuentaDAO;
+import ec.edu.ups.est.ProyectoFinal.dao.PolizaDAO;
+import ec.edu.ups.est.ProyectoFinal.dao.UsuarioDAO;
+import ec.edu.ups.est.ProyectoFinal.model.Cuenta;
+import ec.edu.ups.est.ProyectoFinal.model.Poliza;
+import ec.edu.ups.est.ProyectoFinal.model.Usuario;
+
+@Stateless
+public class PolizaON implements PolizaONLocal {
+	@Inject
+	private PolizaDAO polizaDAO;
+	
+	@Inject
+	private UsuarioDAO usuarioDAO;
+	
+	@Inject
+	private CuentaDAO cuentaDAO;
+	
+	public void solicitarPoliza(double montoCobrar, int numeroCuotas, String cedulaPersona) throws Exception {
+		Poliza poliza = new Poliza();
+		poliza.setInteres(0.05);
+		poliza.setMontoSolicitado(montoCobrar);
+		poliza.setFecha(new Date());
+		poliza.setPlazosPoliza(numeroCuotas);
+		poliza.setEstaAprobado(false);
+		Usuario usuario = usuarioDAO.read(cedulaPersona);
+		if (usuario == null) {
+			throw new Exception("El usuario para la poliza no existe");
+		}
+		poliza.setUsuario(usuario);
+		polizaDAO.insert(poliza);
+	}
+	
+	public void aprobarPoliza(int idPoliza) {
+		Poliza polizaGuardado = polizaDAO.read(idPoliza);
+		polizaGuardado.setEstaAprobado(true);
+		Usuario usuarioPoliza = polizaGuardado.getUsuario();
+		Cuenta cuentaUsuario = usuarioPoliza.getCuenta();
+		cuentaUsuario.setSaldo(cuentaUsuario.getSaldo() + polizaGuardado.getMontoCobrar());
+		polizaDAO.upgrade(polizaGuardado);
+		cuentaDAO.upgrade(cuentaUsuario);
+	}
+	
+	public List<Poliza> getPolizas() {
+		return polizaDAO.getList();
+	}
+	
+	public Poliza getPoliza(int id) {
+		return polizaDAO.read(id);
+	}
+}
