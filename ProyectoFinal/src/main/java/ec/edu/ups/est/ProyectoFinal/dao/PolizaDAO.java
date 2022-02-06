5b62986f8e95985fff244554d357f93992423476
Darwin <dleont@est.ups.edu.ec>
2022-02-06 02:02:43 -0500

    Create PolizaDAO

---
+++ ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/dao/PolizaDAO.java
@@ -0,0 +1,44 @@
+package ec.edu.ups.est.ProyectoFinal.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import ec.edu.ups.est.ProyectoFinal.model.Poliza;
+
+
+@Stateless
+public class PolizaDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	public void insert(Poliza op) {
+		em.persist(op);
+	}
+	
+	public void upgrade(Poliza op) {
+		em.merge(op);
+	}
+	
+	public Poliza read(int codigo ) {
+		Poliza op = em.find(Poliza.class, codigo);
+		return op;
+	}
+
+	public List<Poliza> getList(){
+		List<Poliza> listado = new ArrayList<Poliza>();
+		
+		String jpql="SELECT op FROM Giro op";
+		Query query = em.createQuery(jpql, Poliza.class);
+		listado = query.getResultList();
+		
+		return listado;
+	}
+	
+	
+}
