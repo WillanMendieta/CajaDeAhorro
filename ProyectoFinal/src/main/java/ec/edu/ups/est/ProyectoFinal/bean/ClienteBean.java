f1b28974e3ff5729cf24013333b96c4041c6358d
VásquezByron1 <bvasquezs@est.ups.edu.ec>
2022-02-09 22:18:13 -0500

    Creacion Cuenta Arreglos Reunion

--- ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/bean/ClienteBean.java
+++ ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/bean/ClienteBean.java
@@ -8,8 +8,10 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import ec.edu.ups.est.ProyectoFinal.business.CajeroONLocal;
+import ec.edu.ups.est.ProyectoFinal.business.CuentasONLocal;
 import ec.edu.ups.est.ProyectoFinal.business.UsuarioONLocal;
 import ec.edu.ups.est.ProyectoFinal.model.Cajero;
+import ec.edu.ups.est.ProyectoFinal.model.Cuenta;
 import ec.edu.ups.est.ProyectoFinal.model.Usuario;
 
 @Named
@@ -17,7 +19,11 @@ import ec.edu.ups.est.ProyectoFinal.model.Usuario;
 public class ClienteBean {
  
 	private Usuario newusuario = new Usuario();
+	private Cuenta newcuenta = new Cuenta();
 	private List<Usuario> usuarios;
+	private double saldo;
+	private String numerCuenta;
+	private String tipoCuenta;
 	
 	public Usuario getNewUsuario() {
 		return newusuario;
@@ -33,24 +39,58 @@ public class ClienteBean {
 	@Inject
 	private CajeroONLocal cajeroON;
 	
+	@Inject
+	private CuentasONLocal cuentasON;
+	
 	@PostConstruct
 	public void init() {
 		usuarios = usuarioON.getUsuarios();
 		
 	}
 	
+	
+	
 	public String guardarCliente() {
+		
+		String numeroCuentaValidar = generarNumCuenta();
+		
+		Cuenta cuenta = cuentasON.getCuenta(numeroCuentaValidar);
+		
+		while(cuenta!= null) {
+			numeroCuentaValidar = generarNumCuenta();
+			cuenta = cuentasON.getCuenta(numeroCuentaValidar);
+		}
+		newcuenta.setSaldo(saldo);
+		newcuenta.setNumeroCuenta(generarNumCuenta());
+		newcuenta.setTipoCuenta(tipoCuenta);
+		newcuenta.setUsuario(newusuario);
 		try {
-			this.newusuario.setTipoUsuario("Usuario");
+			
+			this.newusuario.setTipoUsuario("Usuario");			
 			usuarioON.insertarUsuario(newusuario);
+			cuentasON.insertarCuenta(newcuenta);
+
 		}
 		catch(Exception e){
 			e.printStackTrace();
 			return "mensaje-error?faces-redirect=true&texto=" + e.getMessage();
 		}
 		
-		return "mensaje-exito?faces-redirect=true&texto=Cliente Creado con exito";
+		return "mensaje-exito?faces-redirect=true&texto=Se ha creado el usuario y se ha generado la cuenta "+ newcuenta.getNumeroCuenta()+" con éxito";
 	}
+	
+	public String generarNumCuenta() {
+		
+		int valorinicial=11111;
+		int valorFinal = 99999;		
+		int numeroGenerado = (int)(Math.random()*(valorFinal-valorinicial)+valorinicial);
+		String numeroCuentaFinal =Integer.toString(numeroGenerado);
+		return numeroCuentaFinal;
+
+	}
+	
+	
+	
 	
 	public String guardarClienteAdmin() {
 		try {
@@ -97,5 +137,31 @@ public class ClienteBean {
 	public void setUsuarios(List<Usuario> usuarios) {
 		this.usuarios = usuarios;
 	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getNumerCuenta() {
+		return numerCuenta;
+	}
+
+	public void setNumerCuenta(String numerCuenta) {
+		this.numerCuenta = numerCuenta;
+	}
+
+	public String getTipoCuenta() {
+		return tipoCuenta;
+	}
+
+	public void setTipoCuenta(String tipoCuenta) {
+		this.tipoCuenta = tipoCuenta;
+	}
+	
+	
 
 }
