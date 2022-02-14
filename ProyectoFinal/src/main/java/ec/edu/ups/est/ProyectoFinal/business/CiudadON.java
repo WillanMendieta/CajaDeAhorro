47313e0df0681c62d26a4b5ace963b0d94c3de74
Willan Mendieta <wmendietam@est.ups.edu.ec>
2022-02-14 13:41:04 -0500

    Se agrega comentarios en la clase ciudad.
    Se describe los métodos describiendo datos de entrada y salida.

--- ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/CiudadON.java
+++ ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/CiudadON.java
@@ -14,19 +14,30 @@ public class CiudadON {
 	@Inject
 	private CiudadDAO ciudadDAO;
 	
-	
+	/*
+	 * El metodo getUsuario tiene como parametros codigo (ID del usuario).
+	 * Este metodo devuelve a un usuario que conincida con su id de lo contrario devuelve un null.
+	 */
 
 	public Ciudad getUsuario(int codigo) {
 		return ciudadDAO.read(codigo);
 	}
 
 	
+	/*
+	 * El metodo guardarCiudad tiene como parametros de ingreso el codigo de la idudad y el interez del mismo.
+	 * Este metodo actualiza el interez de una ciudad.
+	 */
 	public void guardarCiudad(int codigoCiudad ,  double interes) {
 		Ciudad ciudad = ciudadDAO.read(codigoCiudad);
 		ciudad.setId(codigoCiudad);
 		ciudad.setInteres(interes);		
 		ciudadDAO.update(ciudad);
 	}
+	
+	/*
+	 * El metodo listarCiudades lista todas las ciudades que se encuentran en la base de datos.
+	 */
 	
 	public List<Ciudad> listarCiudades() {
 		return ciudadDAO.getList();
