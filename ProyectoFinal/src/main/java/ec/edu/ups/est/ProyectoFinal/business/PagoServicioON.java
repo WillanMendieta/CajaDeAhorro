9888ee79ce3625b7b1797384fcf58329793a4a12
Willan Mendieta <wmendietam@est.ups.edu.ec>
2022-02-14 16:49:15 -0500

    Se agrega comentarios a la clase PagoServicioON

--- ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/PagoServicioON.java
+++ ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/PagoServicioON.java
@@ -27,8 +27,11 @@ public class PagoServicioON implements PagoServicioONLocal {
 	@Inject
 	private UsuarioDAO usuarioDAO;
 	
-
 	
+	/*
+	 * El metodo guardarPago rcibe como parametros el monto, el servicio y la cedula de la persona
+	 * verifica que la persona exista he ingresa el servicio al usuario.
+	 */
 	public void guardarPago(double monto,String servicio, String cedulaPersona) throws Exception {
 		PagoServicio ps= new PagoServicio();
 		ps.setMonto(monto);
@@ -45,6 +48,11 @@ public class PagoServicioON implements PagoServicioONLocal {
 		usuarioDAO.upgrade(usuario);
 	}
 	
+	/*
+	 * El metodo pagar servicio tiene como parametros el id de pago el cual busca en la base de datos 
+	 * he actualiza a pagago en el campo correspidiente
+	 */
+	
 	public void pagarServicio(int idPago)  throws Exception {		
 		PagoServicio ps=servicioDAO.read(idPago);
 		Usuario usuario=ps.getUsuario();
@@ -58,11 +66,18 @@ public class PagoServicioON implements PagoServicioONLocal {
 			cuentaDAO.upgrade(cuenta);
 		
 	}
+	
+	/*
+	 * El metodo getPagoServicios nos retorna un listado de todos los servicios que se tenga en la base de datos.
+	 */
 
 	public List<PagoServicio> getPagoServicios() {
 		return servicioDAO.getList();
 	}
 	
+	/*
+	 * El metodo getPagoServicio tiene como parametro el id del servicio donde se obtiene un objeto de  PagoServicio con sus datos respectivos
+	 */
 
 	public PagoServicio getPagoServicio(int id) {
 		return servicioDAO.read(id);
