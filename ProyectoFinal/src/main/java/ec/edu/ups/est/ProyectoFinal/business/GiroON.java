2995b32aac4e7a65197ebc9f2c286f9c0b50e024
Willan Mendieta <wmendietam@est.ups.edu.ec>
2022-02-14 14:20:52 -0500

    SE agrega comentarios a la clase GiroON

--- ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/GiroON.java
+++ ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/GiroON.java
@@ -18,20 +18,36 @@ public class GiroON {
 
 	@Inject
 	private CiudadDAO ciudadDAO;
+	
+	/*
+	 * El metodo insertarGiro tiene como parametros el objeto Giro.
+	 * El metodo da como resultado el ingreso del giro dentro de la base de datos.
+	 */
 
 	public void insertarGiro(Giro g) throws Exception {
 		giroDAO.insert(g);
 		
 	}
+	
+	/*
+	 * El metodo getGiros obtiene todos los giros que se encuentran en la base de datos en una lista.
+	 */
 
 	public List<Giro> getGiros() {
 		return giroDAO.getList();
 	}
-
+	
+	/*
+	 * El metodo getGiros tiene como parámetros el id del giro, donde da como resultado el objeto giro.
+	 */
 	public Giro getGiros(int codigo) {
 		return giroDAO.read(codigo);
 	}
-
+	
+	/*
+	 * El metodo getCiudad tiene como parámetros el id de la ciudad.
+	 * Da como resultado al objeto Ciudad obtenido de la base de datos.
+	 */
 	public Ciudad getCiudad(int codigo) {
 
 		Ciudad ciudad = ciudadDAO.read(codigo);
