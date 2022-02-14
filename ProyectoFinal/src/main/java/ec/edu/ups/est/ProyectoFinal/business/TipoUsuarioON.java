62fe58a26a3af93c817b43ba9379e255349ba589
Willan Mendieta <wmendietam@est.ups.edu.ec>
2022-02-14 17:00:21 -0500

    Se agrega comentarios a la clase TipoUsuario

--- ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/TipoUsuarioON.java
+++ ProyectoFinal/src/main/java/ec/edu/ups/est/ProyectoFinal/business/TipoUsuarioON.java
@@ -7,21 +7,28 @@ import ec.edu.ups.est.ProyectoFinal.model.TipoUsuario;
 
 public class TipoUsuarioON {
 
-		@Inject
-		private TipoUsuarioDAO tipoUsuarioDAO;
-		
-		public void crear(TipoUsuario tipoUsuario)  throws Exception {
-			TipoUsuario tipoUsuarioSearched = tipoUsuarioDAO.read(tipoUsuario.getId());
-			if (tipoUsuarioSearched == null) {
-				tipoUsuarioDAO.insert(tipoUsuario);
-				System.out.println("Tipo de usuario insertado");
-			} else {
-				throw new Exception("El tipo de usuario ya existe");
-			}
-		}
-		
-		public TipoUsuario buscar(int id) {
-			return tipoUsuarioDAO.read(id);
+	@Inject
+	private TipoUsuarioDAO tipoUsuarioDAO;
+	
+	/*
+	 * El metodo crear recibe como parametros un objeto tipo usuario
+	 * realiza verificaciones para ver si existe en la base de datos 
+	 * si no es el caso se inserta en la base de datos.
+	 */
+	public void crear(TipoUsuario tipoUsuario) throws Exception {
+		TipoUsuario tipoUsuarioSearched = tipoUsuarioDAO.read(tipoUsuario.getId());
+		if (tipoUsuarioSearched == null) {
+			tipoUsuarioDAO.insert(tipoUsuario);
+			System.out.println("Tipo de usuario insertado");
+		} else {
+			throw new Exception("El tipo de usuario ya existe");
 		}
 	}
-
+	
+	/*
+	 * El metodo buscar, busca en la base de datos por medio del id
+	 */
+	public TipoUsuario buscar(int id) {
+		return tipoUsuarioDAO.read(id);
+	}
+}
